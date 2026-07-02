f316410d11d64a6888dbf78c28e4a030bd55c3b9
VashSt11 <gabyfarias03@gmail.com>
2026-07-01 21:07:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ def init_connection():
 try:
     # Conectar y abrir la hoja
     client = init_connection()
-    SHEET_URL = st.secrets["SPREADSHEET_URL"]
-    sheet = client.open_by_url(SHEET_URL).sheet1
+    SHEET_URL = st.secrets["https://docs.google.com/spreadsheets/d/1MC0tdj5LJn8BtfEdTJjsYjSuk6PDirZejkKQEJlnoYo/edit?usp=sharing"]
+    sheet = client.open_by_url(https://docs.google.com/spreadsheets/d/1MC0tdj5LJn8BtfEdTJjsYjSuk6PDirZejkKQEJlnoYo/edit?usp=sharing).sheet1
 
     # Función para obtener datos frescos
     def get_data():
